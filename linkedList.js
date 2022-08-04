002fd399ce81cb282f522351d840a6f21f81a3eb
Kristina Lubinets <kris.lubinec@gmail.com>
2022-08-04 17:51:37 -0400

    Question 21 on linked list

--- linkedList.js
+++ linkedList.js
@@ -1,4 +1,3 @@
-//Implementing a List Node in js
 class ListNode {
     constructor(data) {
         this.data = data;
@@ -6,20 +5,106 @@ class ListNode {
     }
 }
 
-//Implementing a Linked List in js
-class LinkedList{
+class LinkedList {
     constructor(head = null) {
         this.head = head;
+        this.size = 1;
+    }
+    insertEnd(data) {
+        let newNode = new ListNode(data);
+
+        if(this.head === null) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+
+            // iterate to the end of the list
+            while(current.next) {
+                current = current.next;
+            }
+            //insert node
+            current.next = newNode;
+        }
+        this.size++;
+        return this;
+    }
+    insertHead(data) {
+        let newNode = new ListNode(data);
+
+        if(this.head === null) {
+            this.head = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.size++;
+        return this.head;
+    }
+    insertAtIndex(data, index) {
+        if(index < 0 || this.size < index) return false;
+
+        if(index === 1) return this.insertHead(data);
+        
+        let current = this.head;
+        let previous ;
+        let newNode = new ListNode(data);
+
+        if(this.head !== null) {
+            let len = 1;
+
+            while(len !== index) {
+                previous = current;
+                current = current.next;
+                len++;
+            }
+            newNode.next = current;
+            previous.next = newNode;
+        }
+        this.size++;
+    }
+    length() { 
+        return this.size;
+    }
+    clear() {
+        this.head = null;
+    } 
+    getLast() {
+        let current = this.head;
+        if(current != null) {
+            while(current.next) {
+                current = current.next;
+            }
+        }
+        return current;
+    }
+    getFirst() {
+        if(this.head != null) {
+            return this.head;
+        }
+    }
+    printList() {
+        let current = this.head;
+        if(current != null) {
+            while(current) {
+                console.log('current= ' + current.data)
+                current = current.next;
+            }
+        }
     }
 }
 
+let node1 = new ListNode(1);
+let node3 = new ListNode(3);
+let node2 = new ListNode(2);
 
-let node1 = new ListNode(2);
-let node2 = new ListNode(5);
-
-node1.next = node2;
 
 let list = new LinkedList(node1);
 
-console.log(list.head)
-console.log(list.head.next)
+list.insertEnd(5);
+list.insertEnd(7);
+
+list.insertHead(-1);
+
+list.insertAtIndex(0, 2);
+list.insertAtIndex(200, 4)
+list.printList();
